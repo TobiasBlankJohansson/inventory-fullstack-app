297fb065fd5335c52da46da36609f180dc34ed8e
TobiasBlankJohansson <tobias@annito.se>
2024-12-23 14:11:56 +0100

    feat:add boilercode for search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import { Search } from "./components/search/Search";
 import { Navbar } from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="h-screen">
       <Navbar></Navbar>
+      <Search></Search>
     </div>
   );
 }
