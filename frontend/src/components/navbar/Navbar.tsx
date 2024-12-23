466f5a81ce2f7b8159fb58f2b2620900c3788b98
TobiasBlankJohansson <tobias@annito.se>
2024-12-23 13:58:02 +0100

    feat:add sideMenuShow varible the shows the side menu

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+
 export function Navbar() {
+  const [sideMenuShow, setSideMenuShow] = useState<boolean>(false);
+
   return (
     <>
       <nav className="navbar bg-primary">
         <div className="navbar-start">
           <div className="dropdown">
-            <button className="btn btn-ghost btn-circle" onClick={() => {}}>
+            <button
+              className="btn btn-ghost btn-circle"
+              onClick={() => {
+                setSideMenuShow(true);
+              }}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-8 w-8 text-white"
@@ -27,14 +36,27 @@ export function Navbar() {
         </div>
         <div className="navbar-end"></div>
       </nav>
-      <menu className="absolute top-0 w-4/6 h-screen bg-primary p-4">
-        <h2 className="text-white font-medium text-2xl mb-5">Toobia Nordic</h2>
-        <ol className="text-2xl p-2">
-          <li className="text-white mb-4">Inventory</li>
-          <li className="text-white mb-4">Manage</li>
-          <li className="text-white mb-4"></li>
-        </ol>
-      </menu>
+
+      {sideMenuShow && (
+        <>
+          <menu className="absolute top-0 w-4/6 h-screen bg-primary p-4">
+            <h2 className="text-white font-medium text-2xl mb-5">
+              Toobia Nordic
+            </h2>
+            <ol className="text-2xl p-2">
+              <li className="text-secondary mb-4">Inventory</li>
+              <li className="text-white mb-4">Manage</li>
+              <li className="text-white mb-4"></li>
+            </ol>
+          </menu>
+          <button
+            className="absolute top-0 right-0 w-2/6 h-screen"
+            onClick={() => {
+              setSideMenuShow(false);
+            }}
+          ></button>
+        </>
+      )}
     </>
   );
 }
