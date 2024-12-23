77c5ea4c0c4d079741d69a23d48923ae45718f4c
TobiasBlankJohansson <tobias@annito.se>
2024-12-23 14:16:32 +0100

    feat:add boilercode for Select

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Search } from "./components/search/Search";
 import { Navbar } from "./components/navbar/Navbar";
+import { Select } from "./components/select/Select";
 
 function App() {
   return (
     <div className="h-screen">
       <Navbar></Navbar>
       <Search></Search>
+      <Select></Select>
     </div>
   );
 }
---
+++ frontend/src/components/select/Select.tsx
@@ -0,0 +1,11 @@
+export function Select() {
+  return (
+    <>
+      <select className="select select-bordered w-full max-w-xs">
+        <option>All</option>
+        <option>Verksdag</option>
+        <option>Annex</option>
+      </select>
+    </>
+  );
+}
