9bf977a60c89188d890e9c3845764265f5483e5c
TobiasBlankJohansson <tobias@annito.se>
2024-12-23 18:26:21 +0100

    feat:add tabel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,18 @@ import { Search } from "./components/search/Search";
 import { Navbar } from "./components/navbar/Navbar";
 import { Select } from "./components/select/Select";
 import { Print } from "./components/print/Print";
+import { Tabel } from "./components/tabel/Tabel";
 
 function App() {
   return (
     <div className="h-screen flex flex-col">
       <Navbar></Navbar>
-      <main className="p-2 h-full w-full">
-        <section className="h-10 grid grid-flow-col grid-cols-3 gap-2">
+        <section className="h-10 grid grid-flow-col grid-cols-3 gap-2 m-2 mb-0">
           <Search></Search>
           <Select></Select>
           <Print></Print>
         </section>
-      </main>
+      <Tabel></Tabel>
     </div>
   );
 }
--- frontend/src/components/search/Search.tsx
+++ frontend/src/components/search/Search.tsx
@@ -7,7 +7,7 @@ export function Search() {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 16 16"
           fill="currentColor"
-          className="h-4 w-4 opacity-70"
+          className="h-4 w-4"
         >
           <path
             fillRule="evenodd"
