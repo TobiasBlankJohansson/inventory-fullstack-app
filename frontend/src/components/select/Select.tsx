313f5289a89f9a6ce0dceb624488a3abe3b77111
TobiasBlankJohansson <tobias@annito.se>
2025-01-30 20:56:04 +0100

    feat:add onChange setStorageArea to select

--- frontend/src/components/select/Select.tsx
+++ frontend/src/components/select/Select.tsx
@@ -1,9 +1,16 @@
-export function Select() {
+type inputSelect = {
+  setStorageArea: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export function Select({ setStorageArea }: inputSelect) {
   return (
     <div className="h-10">
-      <select className="select select-bordered w-full max-w-xs min-h-full h-full">
+      <select
+        className="select select-bordered w-full max-w-xs min-h-full h-full"
+        onChange={(e) => setStorageArea(e.target.value)}
+      >
         <option>All</option>
-        <option>Verksdag</option>
+        <option>Verksdagen</option>
         <option>Annex</option>
       </select>
     </div>
