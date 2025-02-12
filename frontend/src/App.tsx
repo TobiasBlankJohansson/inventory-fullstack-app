473e9bbb0dba3f3c49c66abee139fdd198f6fc0c
TobiasBlankJohansson <tobias@annito.se>
2025-02-12 13:18:46 +0100

    feat:add manage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Dashboard } from "./page/Dashboard";
+import { Manage } from "./page/Manage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/manage" element={<Manage />} />
       </Routes>
     </BrowserRouter>
   );
