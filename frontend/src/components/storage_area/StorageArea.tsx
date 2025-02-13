923e3fb93640f122f7d525a1a60f0af5bb3c9abe
TobiasBlankJohansson <Tobias@annito.se>
2025-02-13 13:31:16 +0100

    feat:add importStorageArea

--- frontend/src/components/storage_area/StorageArea.tsx
+++ frontend/src/components/storage_area/StorageArea.tsx
@@ -1,4 +1,8 @@
-export function StorageArea() {
+type importStorageArea = {
+    storageArea: string[];
+}
+
+export function StorageArea({storageArea}:importStorageArea) {
   return (
     <ol className="flex h-12 gap-2 m-2 mb-0 overflow-scroll scrollbar-hide">
       <li className="px-2 w-10 text-xl btn min-h-full h-full">+</li>
