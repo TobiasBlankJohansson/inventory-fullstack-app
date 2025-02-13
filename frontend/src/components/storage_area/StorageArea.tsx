460fbb9afc32c5593f76e50353a1312574e7754a
TobiasBlankJohansson <Tobias@annito.se>
2025-02-13 13:32:38 +0100

    feat:add render logic in StorageArea

--- frontend/src/components/storage_area/StorageArea.tsx
+++ frontend/src/components/storage_area/StorageArea.tsx
@@ -1,15 +1,14 @@
 type importStorageArea = {
-    storageArea: string[];
-}
+  storageArea: string[];
+};
 
-export function StorageArea({storageArea}:importStorageArea) {
+export function StorageArea({ storageArea }: importStorageArea) {
   return (
     <ol className="flex h-12 gap-2 m-2 mb-0 overflow-scroll scrollbar-hide">
       <li className="px-2 w-10 text-xl btn min-h-full h-full">+</li>
-      <li className="px-2 btn min-h-full h-full">Verksdag</li>
-      <li className="px-2 btn min-h-full h-full">Verksdag</li>
-      <li className="px-2 btn min-h-full h-full">Verksdag</li>
-      <li className="px-2 btn min-h-full h-full">Verksdag</li>
+      {storageArea.map((area) => (
+        <li className="px-2 btn min-h-full h-full">{area}</li>
+      ))}
     </ol>
   );
 }
