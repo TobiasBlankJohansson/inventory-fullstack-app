f0b272fda22d6ab5de8fb74ddef7ffcf687905f1
TobiasBlankJohansson <Tobias@annito.se>
2025-02-13 11:25:03 +0100

    feat:add storage list in manage

--- frontend/src/page/Manage.tsx
+++ frontend/src/page/Manage.tsx
@@ -1,5 +1,17 @@
 import { Navbar } from "@/components/navbar/Navbar";
+import { Tabel } from "@/components/tabel/Tabel";
 
 export function Manage() {
-  return <Navbar currentPage={1} currentPageName="Manage"></Navbar>;
+  return (
+    <div className="h-screen flex flex-col">
+      <Navbar currentPage={1} currentPageName="Manage"></Navbar>
+      <ol className="flex h-8 gap-2 m-2 mb-0 overflow-scroll">
+        <li className="px-2 btn min-h-full h-full">+</li>
+        <li className="px-2 btn min-h-full h-full">Verksdag</li>
+        <li className="px-2 btn min-h-full h-full">Verksdag</li>
+        <li className="px-2 btn min-h-full h-full">Verksdag</li>
+        <li className="px-2 btn min-h-full h-full">Verksdag</li>
+      </ol>
+    </div>
+  );
 }
