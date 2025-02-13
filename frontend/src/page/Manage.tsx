17808b2457d00b84a0e6c42840b771fa3da14740
TobiasBlankJohansson <Tobias@annito.se>
2025-02-13 12:13:33 +0100

    feat:add with to add button

--- frontend/src/page/Manage.tsx
+++ frontend/src/page/Manage.tsx
@@ -21,7 +21,7 @@ export function Manage() {
     <div className="h-screen flex flex-col">
       <Navbar currentPage={1} currentPageName="Manage"></Navbar>
       <ol className="flex h-10 gap-2 m-2 mb-0 overflow-scroll">
-        <li className="px-2 btn min-h-full h-full">+</li>
+        <li className="px-2 w-8 btn min-h-full h-full">+</li>
         <li className="px-2 btn min-h-full h-full">Verksdag</li>
         <li className="px-2 btn min-h-full h-full">Verksdag</li>
         <li className="px-2 btn min-h-full h-full">Verksdag</li>
