d13704cf418f2482c19cbe24baa3af4c3f7bdbf2
TobiasBlankJohansson <tobias@annito.se>
2025-02-14 10:55:41 +0100

    refa:refactor storagearea by moving modal to it's own file

---
+++ frontend/src/components/storage_area/AddStorageAreaModal.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+type importAddStorageAreaModal = {
+    setStorageArea: React.Dispatch<React.SetStateAction<string[]>>;
+
+}
+
+export function AddStorageAreaModal({setStorageArea}:importAddStorageAreaModal) {
+    const [addAnotherOne, setAddAnotherOne] = useState<boolean>(false);
+    return <dialog id="storage_area_name_modal" className="modal">
+    <div className="modal-box p-4 w-fit">
+      <form
+        method="dialog"
+        onSubmit={(event) =>
+          SaveStorageArea(
+            setStorageArea,
+            addAnotherOne,
+            setAddAnotherOne,
+            event
+          )
+        }
+      >
+        <h3 className="font-bold text-lg">Storage area creation</h3>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">Storage area name</span>
+          </div>
+          <input
+            id="storage_area_name"
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+            required
+          />
+        </label>
+        <div className="modal-action flex justify-between">
+          <button
+            className="btn"
+            onClick={() => setAddAnotherOne(() => true)}
+          >
+            Add another one
+          </button>
+          <button className="btn">Save</button>
+        </div>
+      </form>
+    </div>
+  </dialog>
+}
+function SaveStorageArea(
+    setStorageArea: React.Dispatch<React.SetStateAction<string[]>>,
+    addAnotherOne: boolean,
+    setAddAnotherOne: React.Dispatch<React.SetStateAction<boolean>>,
+    event?: React.FormEvent
+  ) {
+    if (addAnotherOne) {
+      event?.preventDefault();
+      setAddAnotherOne(() => false);
+    }
+    const storageAreaInput: HTMLInputElement = document.getElementById(
+      "storage_area_name"
+    ) as HTMLInputElement;
+    const storageName = storageAreaInput.value;
+    setStorageArea((storageArea) => [...storageArea, storageName]);
+    storageAreaInput.value = "";
+  }
+  
--- frontend/src/components/storage_area/StorageArea.tsx
+++ frontend/src/components/storage_area/StorageArea.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { AddStorageAreaModal } from "./AddStorageAreaModal";
 
 type importStorageArea = {
   storageArea: string[];
@@ -9,8 +9,6 @@ export function StorageArea({
   storageArea,
   setStorageArea,
 }: importStorageArea) {
-  const [addAnotherOne, setAddAnotherOne] = useState<boolean>(false);
-
   return (
     <>
       <ol className="flex h-12 gap-2 m-2 mb-0 overflow-scroll scrollbar-hide">
@@ -30,63 +28,7 @@ export function StorageArea({
           <li className="px-2 btn min-h-full h-full">{area}</li>
         ))}
       </ol>
-
-      <dialog id="storage_area_name_modal" className="modal">
-        <div className="modal-box p-4 w-fit">
-          <form
-            method="dialog"
-            onSubmit={(event) =>
-              SaveStorageArea(
-                setStorageArea,
-                addAnotherOne,
-                setAddAnotherOne,
-                event
-              )
-            }
-          >
-            <h3 className="font-bold text-lg">Storage area creation</h3>
-            <label className="form-control w-full max-w-xs">
-              <div className="label">
-                <span className="label-text">Storage area name</span>
-              </div>
-              <input
-                id="storage_area_name"
-                type="text"
-                placeholder="Type here"
-                className="input input-bordered w-full max-w-xs"
-                required
-              />
-            </label>
-            <div className="modal-action flex justify-between">
-              <button
-                className="btn"
-                onClick={() => setAddAnotherOne(() => true)}
-              >
-                Add another one
-              </button>
-              <button className="btn">Save</button>
-            </div>
-          </form>
-        </div>
-      </dialog>
+      <AddStorageAreaModal setStorageArea={setStorageArea} />
     </>
   );
-}
-
-function SaveStorageArea(
-  setStorageArea: React.Dispatch<React.SetStateAction<string[]>>,
-  addAnotherOne: boolean,
-  setAddAnotherOne: React.Dispatch<React.SetStateAction<boolean>>,
-  event?: React.FormEvent
-) {
-  if (addAnotherOne) {
-    event?.preventDefault();
-    setAddAnotherOne(() => false);
-  }
-  const storageAreaInput: HTMLInputElement = document.getElementById(
-    "storage_area_name"
-  ) as HTMLInputElement;
-  const storageName = storageAreaInput.value;
-  setStorageArea((storageArea) => [...storageArea, storageName]);
-  storageAreaInput.value = "";
 }
