74b2bfa644adb2a24e855ed99b10dd1787ddbabd
TobiasBlankJohansson <Tobias@annito.se>
2025-03-05 13:33:24 +0100

    faet:add onClick in storage area

--- frontend/src/components/storage_area/StorageArea.tsx
+++ frontend/src/components/storage_area/StorageArea.tsx
@@ -6,6 +6,21 @@ type importStorageArea = {
   setStorageArea: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
+const onClick = (
+  select: boolean,
+  setSelect: React.Dispatch<React.SetStateAction<boolean>>,
+  area: string,
+  setSelected: React.Dispatch<React.SetStateAction<string[]>>,
+  selected: string[]
+) => {
+  setSelect(() => !select);
+  if (!select) {
+    setSelected([...selected, area]);
+  } else {
+    setSelected(selected.filter(item => item !== area));
+  }
+};
+
 export function StorageArea({
   storageArea,
   setStorageArea,
