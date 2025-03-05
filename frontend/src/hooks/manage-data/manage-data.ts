6f38a9e341d1c5b1b5699de304cebe1f146d51af
TobiasBlankJohansson <Tobias@annito.se>
2025-03-05 15:45:48 +0100

    feat:add fetch-items and filter items to manage-data

--- frontend/src/hooks/manage-data/manage-data.ts
+++ frontend/src/hooks/manage-data/manage-data.ts
@@ -1,48 +1,25 @@
-import { getItems } from "@/api/InventoryApiService";
-import { Item } from "@/types";
-import { useState, useEffect, useMemo } from "react";
+import { useState } from "react";
+import { useFilterItems } from "../filter-items";
+import { useFetchItems } from "../fetch-items";
 
 export const useManageData = () => {
-  const [items, setItems] = useState<Item[]>([]);
+  const { items, setItems } = useFetchItems();
   const [storageArea, setStorageArea] = useState<string[]>([
     "Verksdag",
     "Annex",
     "Bothuset",
   ]);
+
   const [selected, setSelected] = useState<string[]>([]);
   const [search, setSearch] = useState<string>("");
   const [checkedItems, setCheckedItems] = useState<string[]>([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getItems();
-        setItems(data);
-      } catch (error) {
-        console.error("Failed to fetch items:", error);
-      }
-    };
-    fetchData();
-  }, []);
+  const itemList = useFilterItems(items, search, selected);
 
   const handleDelete = () => {
     setItems((prev) => prev.filter((item) => !checkedItems.includes(item.id)));
     setCheckedItems([]);
   };
-
-  const itemList = useMemo(() => {
-    return items.filter((item) => {
-      const matchesSearch = item.name
-        .toLowerCase()
-        .includes(search.toLowerCase());
-      const matchesStorage =
-        selected.length === 0 ||
-        selected
-          .map((s) => s.toLowerCase())
-          .includes(item.storageArea.toLowerCase());
-      return matchesSearch && matchesStorage;
-    });
-  }, [search, items, selected]);
 
   return {
     itemList,
