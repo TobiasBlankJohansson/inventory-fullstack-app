7a94049d1acb6f8b81343d1644dbbd5b1671a71e
TobiasBlankJohansson <Tobias@annito.se>
2025-03-06 12:42:44 +0100

    faet:add navbar in itemEditor

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -3,11 +3,11 @@ import { Laptop } from "./laptop/Laptop";
 import { Phone } from "./phone/Phone";
 
 type importNavbar = {
-  currentPage: number;
+  currentPage?: number;
   currentPageName: string;
 };
 
-export function Navbar({ currentPage, currentPageName }: importNavbar) {
+export function Navbar({ currentPage = -1, currentPageName }: importNavbar) {
   const { isLaptop } = useScreen();
   return isLaptop ? Laptop(currentPage) : Phone(currentPageName, currentPage);
 }
---
+++ frontend/src/page/ItemEditor.tsx
@@ -0,0 +1,9 @@
+import { Navbar, ScreenContainer } from "@/components";
+
+export const ItemEditor = () => {
+  return (
+    <ScreenContainer>
+      <Navbar currentPageName="item"></Navbar>
+    </ScreenContainer>
+  );
+};
