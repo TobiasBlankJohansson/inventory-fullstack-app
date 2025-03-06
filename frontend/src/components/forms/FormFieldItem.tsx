a3655d96b8cd7de26860baf85181910eb9f6ddbd
TobiasBlankJohansson <Tobias@annito.se>
2025-03-06 08:22:49 +0100

    faet:add FormFieldItem

---
+++ frontend/src/components/forms/FormFieldItem.tsx
@@ -0,0 +1,40 @@
+import { FormFieldConfig } from "@/types";
+
+export const FormField = ({
+  field,
+  options,
+}: {
+  field: FormFieldConfig;
+  options?: string[];
+}) => (
+  <label className="form-control w-full max-w-xs">
+    <div className="label">
+      <span className="label-text">{field.label}</span>
+    </div>
+    {field.type === "select" ? (
+      <select
+        id={`item_${field.key}`}
+        className="select select-bordered w-full max-w-xs"
+        required
+        defaultValue=""
+      >
+        <option value="" disabled>
+          Select {field.label.toLowerCase()}
+        </option>
+        {options?.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    ) : (
+      <input
+        id={`item_${field.key}`}
+        type={field.type}
+        placeholder={field.placeholder}
+        className="input input-bordered w-full max-w-xs"
+        required
+      />
+    )}
+  </label>
+);
