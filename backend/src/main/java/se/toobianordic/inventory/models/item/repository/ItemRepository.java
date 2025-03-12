19caaaacc4e425917d0f9a7fe9cc4de3bdb43ed5
TobiasBlankJohansson <Tobias@annito.se>
2025-03-12 11:06:40 +0100

    faet:add ItemRepository to extend ListCrudRepository

--- backend/src/main/java/se/toobianordic/inventory/models/item/repository/ItemRepository.java
+++ backend/src/main/java/se/toobianordic/inventory/models/item/repository/ItemRepository.java
@@ -1,4 +1,7 @@
 package se.toobianordic.inventory.models.item.repository;
 
-public class ItemRepository {
+import org.springframework.data.repository.ListCrudRepository;
+import se.toobianordic.inventory.models.item.model.Item;
+
+public interface ItemRepository extends ListCrudRepository<Item, String> {
 }
