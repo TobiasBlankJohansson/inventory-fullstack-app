eeaebefc909b2fc5de4337ec236d2f02910f81f8
TobiasBlankJohansson <Tobias@annito.se>
2025-03-12 11:22:57 +0100

    feat:add ItemService in item controller

--- backend/src/main/java/se/toobianordic/inventory/models/item/controller/ItemController.java
+++ backend/src/main/java/se/toobianordic/inventory/models/item/controller/ItemController.java
@@ -1,4 +1,11 @@
 package se.toobianordic.inventory.models.item.controller;
 
+import se.toobianordic.inventory.models.item.service.ItemService;
+
 public class ItemController {
+    private ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+    }
+
 }
