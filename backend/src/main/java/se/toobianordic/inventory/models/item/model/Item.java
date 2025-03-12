2ef7baa005254c3f9a785d2afacef1af29d3d1fb
TobiasBlankJohansson <Tobias@annito.se>
2025-03-12 11:28:53 +0100

    feat:add Entity and Table to Item

--- backend/src/main/java/se/toobianordic/inventory/models/item/model/Item.java
+++ backend/src/main/java/se/toobianordic/inventory/models/item/model/Item.java
@@ -1,5 +1,8 @@
 package se.toobianordic.inventory.models.item.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +12,10 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "inventory")
 public class Item {
+    @Id
     String id;
     String name;
     String quantity;
