ac3e075275b4927fa78e10e53201f25f17d8298e
TobiasBlankJohansson <Tobias@annito.se>
2025-03-12 11:09:01 +0100

    faet:add getItemList

--- backend/src/main/java/se/toobianordic/inventory/models/item/service/ItemService.java
+++ backend/src/main/java/se/toobianordic/inventory/models/item/service/ItemService.java
@@ -1,6 +1,9 @@
 package se.toobianordic.inventory.models.item.service;
 
+import se.toobianordic.inventory.models.item.model.Item;
 import se.toobianordic.inventory.models.item.repository.ItemRepository;
+
+import java.util.List;
 
 public class ItemService {
     private ItemRepository itemRepository;
@@ -8,5 +11,7 @@ public class ItemService {
     public ItemService(ItemRepository itemRepository) {
     }
 
-
+    public List<Item> getItemList() {
+        return itemRepository.findAll();
+    }
 }
