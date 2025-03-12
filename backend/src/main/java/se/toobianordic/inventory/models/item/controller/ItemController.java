e451323c8009d053d74414ea16cc19afc7b9d8e3
TobiasBlankJohansson <Tobias@annito.se>
2025-03-12 11:39:33 +0100

    feat:add update

--- backend/src/main/java/se/toobianordic/inventory/models/item/controller/ItemController.java
+++ backend/src/main/java/se/toobianordic/inventory/models/item/controller/ItemController.java
@@ -1,7 +1,11 @@
 package se.toobianordic.inventory.models.item.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import se.toobianordic.inventory.models.item.service.ItemService;
 
@@ -12,6 +16,16 @@ public class ItemController {
     private ItemService itemService;
 
     public ItemController(ItemService itemService) {
+    }
+
+    @PostMapping
+    public ResponseEntity<ItemDto> update(@RequestBody ItemDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ItemDto.fromItem(itemService.update(
+                        dto.id(),
+                        dto.name(),
+                        dto.quantity(),
+                        dto.quantity())));
     }
 
 }
--- backend/src/main/java/se/toobianordic/inventory/models/item/controller/ItemDto.java
+++ backend/src/main/java/se/toobianordic/inventory/models/item/controller/ItemDto.java
@@ -1,8 +1,13 @@
 package se.toobianordic.inventory.models.item.controller;
 
+import se.toobianordic.inventory.models.item.model.Item;
+
 public record ItemDto
         (String id,
          String name,
          String quantity,
          String storageArea) {
+    public static ItemDto fromItem(Item item) {
+        return new ItemDto(item.getId(), item.getName(), item.getQuantity(), item.getStorageArea());
+    }
 }
