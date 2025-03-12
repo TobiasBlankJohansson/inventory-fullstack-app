accc357def4acdf563d2b217321817c7edaf4128
TobiasBlankJohansson <Tobias@annito.se>
2025-03-12 11:07:46 +0100

    faet:add itemRepository in ItemService

--- backend/src/main/java/se/toobianordic/inventory/models/item/service/ItemService.java
+++ backend/src/main/java/se/toobianordic/inventory/models/item/service/ItemService.java
@@ -1,4 +1,12 @@
 package se.toobianordic.inventory.models.item.service;
 
+import se.toobianordic.inventory.models.item.repository.ItemRepository;
+
 public class ItemService {
+    private ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+    }
+
+
 }
