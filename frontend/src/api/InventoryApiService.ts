90ced40e6c6c0f7074f9766fd3732bebf3aaf995
TobiasBlankJohansson <tobias@annito.se>
2025-03-14 16:31:41 +0100

    feat:add correct port

--- frontend/src/api/InventoryApiService.ts
+++ frontend/src/api/InventoryApiService.ts
@@ -1,13 +1,13 @@
 import { Item } from "@/types";
 
 export async function getItems(): Promise<Item[]> {
-  return await fetch("http://localhost:3001/api/v1/inventory").then((res) =>
+  return await fetch("http://localhost:3011/api/v1/inventory").then((res) =>
     res.json()
   );
 }
 
 export async function postItem(item: Item): Promise<Item> {
-  return fetch("http://localhost:3001/api/v1/inventory", {
+  return fetch("http://localhost:3011/api/v1/inventory", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(item),
@@ -15,7 +15,7 @@ export async function postItem(item: Item): Promise<Item> {
 }
 
 export async function putItem(item: Item): Promise<Item> {
-  return fetch("http://localhost:3001/api/v1/inventory", {
+  return fetch("http://localhost:3011/api/v1/inventory", {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(item),
@@ -23,14 +23,14 @@ export async function putItem(item: Item): Promise<Item> {
 }
 
 export async function deleteItem(id: string): Promise<boolean> {
-  return fetch("http://localhost:3001/api/v1/inventory/" + id, {
+  return fetch("http://localhost:3011/api/v1/inventory/" + id, {
     method: "POST",
   }).then((res) => res.json());
 }
 
 export async function getStorageArea(): Promise<string[]> {
   return mockDataStorageArea;
-  return await fetch("http://localhost:3001/api/items").then((res) =>
+  return await fetch("http://localhost:3011/api/items").then((res) =>
     res.json()
   );
 }
