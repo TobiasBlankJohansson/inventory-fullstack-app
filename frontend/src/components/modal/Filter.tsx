f28a1a1880e97784af5e49fcf4b1b9ca6196f9f5
TobiasBlankJohansson <tobias@annito.se>
2025-03-16 18:20:28 +0100

    faet:add responeible in filter

--- frontend/src/components/modal/Filter.tsx
+++ frontend/src/components/modal/Filter.tsx
@@ -3,16 +3,29 @@ import { StorageArea } from "../storage_area";
 type Prop = {
   setSelected: React.Dispatch<React.SetStateAction<string[]>>;
   storageArea: string[];
+  setResponsible: React.Dispatch<React.SetStateAction<string[]>>;
+  responsible: string[];
 };
 
-export const Filter = ({ setSelected, storageArea }: Prop) => {
+export const Filter = ({
+  setSelected,
+  storageArea,
+  setResponsible,
+  responsible,
+}: Prop) => {
   return (
     <dialog id="filter" className="modal">
       <div className="modal-box">
-        <h3 className="font-bold text-lg mb-5">Filter</h3>
+        <h3 className="font-bold text-lg">Filter</h3>
+        <p className="mt-2">Storage area</p>
         <StorageArea
           setSelected={setSelected}
           storageArea={storageArea}
+        ></StorageArea>
+        <p className="mt-5">Responsible</p>
+        <StorageArea
+          setSelected={setResponsible}
+          storageArea={responsible}
         ></StorageArea>
       </div>
       <form method="dialog" className="modal-backdrop">
