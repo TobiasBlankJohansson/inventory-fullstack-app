5464e53e4bfac5e5d60e325d29d3457558f3ae6c
TobiasBlankJohansson <tobias@annito.se>
2025-03-16 18:08:55 +0100

    faet:add StorageArea to filter

--- frontend/src/components/modal/Filter.tsx
+++ frontend/src/components/modal/Filter.tsx
@@ -1,9 +1,21 @@
-export const Filter = () => {
+import { StorageArea } from "../storage_area";
+
+type Prop = {
+  setSelected: React.Dispatch<React.SetStateAction<string[]>>;
+  setStorageArea: (updateFn: (prevStorage: string[]) => string[]) => void;
+  storageArea: string[];
+};
+
+export const Filter = ({ setSelected, setStorageArea, storageArea }: Prop) => {
   return (
     <dialog id="filter" className="modal">
       <div className="modal-box">
-        <h3 className="font-bold text-lg">Hello!</h3>
-        <p className="py-4">Press ESC key or click outside to close</p>
+        <h3 className="font-bold text-lg">Filter</h3>
+        <StorageArea
+          setSelected={setSelected}
+          setStorageArea={setStorageArea}
+          storageArea={storageArea}
+        ></StorageArea>
       </div>
       <form method="dialog" className="modal-backdrop">
         <button>close</button>
