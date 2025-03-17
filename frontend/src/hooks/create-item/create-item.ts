e81485a128854477df99d7928df7fec1ac9712b6
TobiasBlankJohansson <tobias@annito.se>
2025-03-17 17:55:57 +0100

    feat:add controll of form after change in item

--- frontend/src/hooks/create-item/create-item.ts
+++ frontend/src/hooks/create-item/create-item.ts
@@ -1,44 +1,47 @@
-import {FormEvent, useState} from "react";
-import {FORM_FIELDS_ITEM} from "@/constants.ts";
-import {Item} from "@/types";
-import {usePostItem} from "@/hooks/post-item";
+import { FormEvent, useState } from "react";
+import { FORM_FIELDS_ITEM } from "@/constants.ts";
+import { Item } from "@/types";
+import { usePostItem } from "@/hooks/post-item";
 
-export const useCreateItem = <T extends Item>(setItems: (updateFn: (prevItems: T[]) => T[]) => void) => {
+export const useCreateItem = <T extends Item>(
+  setItems: (updateFn: (prevItems: T[]) => T[]) => void
+) => {
   const [addAnotherOne, setAddAnotherOne] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const {mutate} = usePostItem(setErrorMessage);
+  const { mutate } = usePostItem(setErrorMessage);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const newItem = Object.fromEntries(
-      FORM_FIELDS_ITEM.map(({key}) => [key, formData.get(`item_${key}`)])
+      FORM_FIELDS_ITEM.map(({ key }) => [key, formData.get(`item_${key}`)])
     ) as T;
-    if (!newItem.id || !newItem.name || !newItem.quantity || !newItem.storageArea) {
+    newItem.id = "5";
+
+    if (
+      !newItem.equipment ||
+      !newItem.quantity ||
+      !newItem.storageArea ||
+      !newItem.responsible
+    ) {
       setErrorMessage("Please fill in all required fields");
       return;
     }
-    if (newItem.id.length != 6) {
-      setErrorMessage("Id need to be 6 characters");
-      return;
-    }
-    if (parseInt(newItem.quantity) < 0) {
+
+    if (parseInt(newItem.quantity) < 1) {
       setErrorMessage("Quantity can't be negative");
       return;
     }
 
     const form = e.currentTarget;
     setItems((prevItems) => {
-      const isIdUnique = !prevItems.some((item) => item.id === newItem.id);
-      const isNameUnique = !prevItems.some((item) => item.name === newItem.name);
+      const isUnique = !prevItems.some(
+        (item) =>
+          item.id === newItem.id && item.responsible === newItem.responsible
+      );
 
-      if (!isIdUnique) {
-        setErrorMessage(`Item with ID "${newItem.id}" already exists`);
-        return prevItems;
-      }
-
-      if (!isNameUnique) {
-        setErrorMessage(`Item with name "${newItem.name}" already exists`);
+      if (!isUnique) {
+        setErrorMessage("Item already exists with equipment and responsible");
         return prevItems;
       }
 
@@ -61,7 +64,7 @@ export const useCreateItem = <T extends Item>(setItems: (updateFn: (prevItems: T
 
       return updatedItems;
     });
-  }
+  };
 
-  return {errorMessage, handleSubmit, setAddAnotherOne};
+  return { errorMessage, handleSubmit, setAddAnotherOne };
 };
