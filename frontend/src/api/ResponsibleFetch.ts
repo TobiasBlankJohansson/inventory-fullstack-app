0682703dcaf14d7749ec967d9576a440560522a8
TobiasBlankJohansson <Tobias@annito.se>
2025-03-18 08:09:15 +0100

    faet:add Responsible type in Responsible Fetch

--- frontend/src/api/ResponsibleFetch.ts
+++ frontend/src/api/ResponsibleFetch.ts
@@ -1,6 +1,8 @@
+import {Responsible} from "@/types";
+
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-export async function getResponsible(): Promise<string[]> {
+export async function getResponsible(): Promise<Responsible[]> {
   return mockDataResponsible;
   return await fetch(BACKEND_URL + "/api/items").then((res) => res.json());
 }
@@ -12,15 +14,15 @@ export async function postResponsible(responsible: string): Promise<boolean> {
   return true;
 }
 
-const mockDataResponsible: string[] = [
-  "John Doe",
-  "Jane Smith",
-  "Emily Johnson",
-  "Michael Brown",
-  "Sarah Wilson",
-  "David Miller",
-  "Jessica Davis",
-  "Daniel Martinez",
-  "Olivia Taylor",
-  "James Anderson",
+const mockDataResponsible: Responsible[] = [
+  {id: "1", name: "John Doe"},
+  {id: "2", name: "Jane Smith"},
+  {id: "3", name: "Emily Johnson"},
+  {id: "4", name: "Michael Brown"},
+  {id: "5", name: "Sarah Wilson"},
+  {id: "6", name: "David Miller"},
+  {id: "7", name: "Jessica Davis"},
+  {id: "8", name: "Daniel Martinez"},
+  {id: "9", name: "Olivia Taylor"},
+  {id: "10", name: "James Anderson"},
 ];
