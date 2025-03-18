d449172291bae5bfd99c05861b9270dda59a2e75
TobiasBlankJohansson <Tobias@annito.se>
2025-03-18 08:07:57 +0100

    faet:add Equipment type in Equipment Fetch

--- frontend/src/api/EquipmentFetch.ts
+++ frontend/src/api/EquipmentFetch.ts
@@ -1,6 +1,8 @@
+import {Equipment} from "@/types";
+
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-export async function getEquipment(): Promise<string[]> {
+export async function getEquipment(): Promise<Equipment[]> {
   return mockDataEquipment;
   return await fetch(BACKEND_URL + "/api/items").then((res) => res.json());
 }
@@ -12,15 +14,15 @@ export async function postEquipment(equipment: string): Promise<boolean> {
   return true;
 }
 
-const mockDataEquipment: string[] = [
-  "Compass",
-  "Multi-tool Knife",
-  "First Aid Kit",
-  "Flashlight",
-  "Sleeping Bag",
-  "Fire Starter",
-  "Water Bottle",
-  "Backpack",
-  "Hiking Boots",
-  "Rope",
+const mockDataEquipment: Equipment[] = [
+  {id: "1", name: "Compass"},
+  {id: "2", name: "Multi-tool Knife"},
+  {id: "3", name: "First Aid Kit"},
+  {id: "4", name: "Flashlight"},
+  {id: "5", name: "Sleeping Bag"},
+  {id: "6", name: "Fire Starter"},
+  {id: "7", name: "Water Bottle"},
+  {id: "8", name: "Backpack"},
+  {id: "9", name: "Hiking Boots"},
+  {id: "10", name: "Rope"},
 ];
--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,6 +1,4 @@
 export * from "./fetch";
 export * from "./filter-items";
 export * from "./page-data";
-export * from "./fetch/item/delete-item";
-export * from "./fetch/item/put-item";
 export * from "./create-item";
