370cc57870f2524cc15b11a3ef55e572fb304195
TobiasBlankJohansson <Tobias@annito.se>
2025-03-18 08:10:44 +0100

    faet:add Storage type in Storage Fetch

--- frontend/src/api/StorageFetch.ts
+++ frontend/src/api/StorageFetch.ts
@@ -1,6 +1,8 @@
+import {Storage} from "@/types";
+
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
-export async function getStorageArea(): Promise<string[]> {
+export async function getStorageArea(): Promise<Storage[]> {
   return mockDataStorageArea;
   return await fetch(BACKEND_URL + "/api/items").then((res) => res.json());
 }
@@ -12,4 +14,10 @@ export async function postStorageArea(storageArea: string): Promise<boolean> {
   return true;
 }
 
-const mockDataStorageArea: string[] = ["Annex", "Verksdag"];
+const mockDataStorageArea: Storage[] = [
+  {id: "1", name: "Tool Shed"},
+  {id: "2", name: "Warehouse A"},
+  {id: "3", name: "Kitchen Storage"},
+  {id: "4", name: "Garage"},
+  {id: "5", name: "Emergency Room"},
+];
