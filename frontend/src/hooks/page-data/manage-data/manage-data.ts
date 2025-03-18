d69b53a41b7d9afe76915bc762582752069bcba3
TobiasBlankJohansson <Tobias@annito.se>
2025-03-18 11:54:27 +0100

    feat:add generic saveAsset

--- frontend/src/hooks/page-data/manage-data/manage-data.ts
+++ frontend/src/hooks/page-data/manage-data/manage-data.ts
@@ -9,7 +9,6 @@ import {
 } from "@/hooks";
 import {consolidateInventory, openModal} from "@/util";
 
-
 export const useManageData = () => {
   const {items, setItems} = useGetItems();
   const {mutateAsync} = usePostStorage();
@@ -30,26 +29,30 @@ export const useManageData = () => {
 
   const handleCreate = () => openModal("create_item");
 
-  async function saveAsset(
-    formId: string,
-    addAnotherOne: boolean,
-    setAddAnotherOne: React.Dispatch<React.SetStateAction<boolean>>,
-    event?: React.FormEvent
-  ) {
-    const input: HTMLInputElement = document.getElementById(
-      "input " + formId
-    ) as HTMLInputElement;
-    const name = input.value;
-    const data = await mutateAsync(name);
-    if (data) {
-      setStorageArea((storageArea) => [...storageArea, data]);
-    }
-    input.value = "";
-    if (addAnotherOne) {
-      event?.preventDefault();
-      setAddAnotherOne(() => false);
+  const saveAsset = <T>(set: (updateFn: (prevData: T[]) => T[]) => void,
+                        mutateAsync: (variables: string) => Promise<T>) => {
+    return async function saveAsset(
+      formId: string,
+      addAnotherOne: boolean,
+      setAddAnotherOne: React.Dispatch<React.SetStateAction<boolean>>,
+      event?: React.FormEvent
+    ) {
+      const input: HTMLInputElement = document.getElementById(
+        "input " + formId
+      ) as HTMLInputElement;
+      const name = input.value;
+      const data = await mutateAsync(name);
+      if (data) {
+        set((prevData) => [...prevData, data]);
+      }
+      input.value = "";
+      if (addAnotherOne) {
+        event?.preventDefault();
+        setAddAnotherOne(() => false);
+      }
     }
   }
+
 
   return {
     options: {equipment, responsible, storageArea},
