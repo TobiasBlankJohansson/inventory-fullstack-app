90776a8bef75bdf4315f99f740d8cda4fdc539ea
TobiasBlankJohansson <Tobias@annito.se>
2025-03-19 11:21:37 +0100

    faet:add EquipmentFromList

--- frontend/src/types/equipment.ts
+++ frontend/src/types/equipment.ts
@@ -2,3 +2,7 @@ export type Equipment = {
   id: string;
   name: string;
 };
+
+export const EquipmentFromList = (equipmentName: string, list: Equipment[]) => {
+  return list.find((equipment) => equipment.name === equipmentName);
+}
