85704dc1ae2264eb2b1c39d30f944b623f9ed077
TobiasBlankJohansson <Tobias@annito.se>
2025-03-19 11:22:59 +0100

    feat:add responsibleFromList

--- frontend/src/types/equipment.ts
+++ frontend/src/types/equipment.ts
@@ -3,6 +3,6 @@ export type Equipment = {
   name: string;
 };
 
-export const EquipmentFromList = (equipmentName: string, list: Equipment[]) => {
+export const equipmentFromList = (equipmentName: string, list: Equipment[]) => {
   return list.find((equipment) => equipment.name === equipmentName);
 }
--- frontend/src/types/responsible.ts
+++ frontend/src/types/responsible.ts
@@ -2,3 +2,7 @@ export type Responsible = {
   id: string;
   name: string;
 };
+
+export const responsibleFromList = (responsibleName: string, list: Responsible[]) => {
+  return list.find((equipment) => equipment.name === responsibleName);
+}
