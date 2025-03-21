2dd521144627d0b607a325eff2efe7d445832628
TobiasBlankJohansson <Tobias@annito.se>
2025-03-21 13:30:34 +0100

    feat:add fetch for storage to backend

--- frontend/src/api/StorageFetch.ts
+++ frontend/src/api/StorageFetch.ts
@@ -3,15 +3,19 @@ import {Storage} from "@/types";
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export async function getStorageArea(): Promise<Storage[]> {
-  return mockDataStorageArea;
-  return await fetch(BACKEND_URL + "/api/items").then((res) => res.json());
+  const storages = await fetch(BACKEND_URL + "/api/storages").then((res) => res.json())
+  return storages.storages;
 }
 
 export async function postStorageArea(storageArea: { name: string }): Promise<Storage> {
-  return {id: "0", name: storageArea.name};
+  return await fetch(BACKEND_URL + "/api/storages", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(storageArea.name),
+  }).then((res) => res.json());
 }
 
-const mockDataStorageArea: Storage[] = [
+export const mockDataStorageArea: Storage[] = [
   {id: "1", name: "Tool Shed"},
   {id: "2", name: "Warehouse A"},
   {id: "3", name: "Kitchen Storage"},
