7df8aa9df42abd741db7f14b906f039c3335ea65
TobiasBlankJohansson <Tobias@annito.se>
2025-03-24 07:50:17 +0100

    feat:add error message from wrong input in EquipmentDto

--- backend/src/main/java/toobia/se/inventory/modules/equipment/controller/dtos/EquipmentDto.java
+++ backend/src/main/java/toobia/se/inventory/modules/equipment/controller/dtos/EquipmentDto.java
@@ -5,5 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public record EquipmentDto(
         String name,
-        @Size(min = 6, max = 6) @Pattern(regexp = "\\d{6}") String id) {
+        @Size(min = 6, max = 6, message = "Id need to be 6 characters long")
+        @Pattern(regexp = "\\d{6}", message = "Id need to be numbers")
+         String id) {
 }
