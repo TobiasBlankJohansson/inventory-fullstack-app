0b46093349d5aee0d30012b3030e74449daaa6ed
TobiasBlankJohansson <Tobias@annito.se>
2025-03-24 08:43:47 +0100

    feat:add lombok anotations in Responsible

--- backend/src/main/java/toobia/se/inventory/modules/responsible/model/Responsible.java
+++ backend/src/main/java/toobia/se/inventory/modules/responsible/model/Responsible.java
@@ -1,15 +1,20 @@
 package toobia.se.inventory.modules.responsible.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import toobia.se.inventory.modules.item.model.Item;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-
 @Entity
 @Table(name = "responsible")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Responsible {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -18,35 +23,19 @@ public class Responsible {
     private String name;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "responsible")
-    private List<Item> items;
+    private List<Item> items = new ArrayList<>();
 
     public Responsible(String name) {
         this.name = name;
     }
 
-    public Responsible() {}
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
     public void addItem(Item item) {
         items.add(item);
+        item.setResponsible(this);
     }
 
     public void removeItem(Item item) {
         items.remove(item);
+        item.setResponsible(null);
     }
 }
