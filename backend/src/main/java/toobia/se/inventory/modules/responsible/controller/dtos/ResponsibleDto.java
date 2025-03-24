054bd48ed6e40b82f0ff94560702915b19a52c93
TobiasBlankJohansson <Tobias@annito.se>
2025-03-24 08:36:29 +0100

    faet:add from and listFrom in ResponsibleDto

--- backend/src/main/java/toobia/se/inventory/modules/responsible/controller/dtos/ResponsibleDto.java
+++ backend/src/main/java/toobia/se/inventory/modules/responsible/controller/dtos/ResponsibleDto.java
@@ -1,6 +1,18 @@
 package toobia.se.inventory.modules.responsible.controller.dtos;
 
+import toobia.se.inventory.modules.equipment.controller.dtos.EquipmentDto;
+import toobia.se.inventory.modules.equipment.model.Equipment;
+import toobia.se.inventory.modules.responsible.model.Responsible;
+
+import java.util.List;
 import java.util.UUID;
 
 public record ResponsibleDto(UUID id, String name) {
+
+    public static ResponsibleDto from(Responsible responsible) {
+        return new ResponsibleDto(responsible.getEquipmentId(),responsible.getEquipmentName());
+    }
+    public static List<ResponsibleDto> listFrom(List<Responsible> responsible) {
+        return responsible.stream().map(ResponsibleDto::from).toList();
+    }
 }
