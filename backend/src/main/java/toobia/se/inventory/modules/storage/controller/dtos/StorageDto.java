d88a9cfcab1aa61bb554364caba00c419e92fe4f
TobiasBlankJohansson <Tobias@annito.se>
2025-03-24 09:47:19 +0100

    feat:add from and listFrom in StorageDto

--- backend/src/main/java/toobia/se/inventory/modules/storage/controller/dtos/StorageDto.java
+++ backend/src/main/java/toobia/se/inventory/modules/storage/controller/dtos/StorageDto.java
@@ -1,6 +1,17 @@
 package toobia.se.inventory.modules.storage.controller.dtos;
 
+import toobia.se.inventory.modules.responsible.controller.dtos.ResponsibleDto;
+import toobia.se.inventory.modules.responsible.model.Responsible;
+import toobia.se.inventory.modules.storage.model.Storage;
+
+import java.util.List;
 import java.util.UUID;
 
-public record StorageDto(String name, UUID id) {
+public record StorageDto(UUID id,String name) {
+    public static StorageDto from(Storage storage) {
+        return new StorageDto(storage.getId(),storage.getName());
+    }
+    public static List<StorageDto> listFrom(List<Storage> storage) {
+        return storage.stream().map(StorageDto::from).toList();
+    }
 }
