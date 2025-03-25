de23174bdeb9e6d0bceda894e2ce11a1c6f689f7
TobiasBlankJohansson <Tobias@annito.se>
2025-03-25 08:26:31 +0100

    feat:add MethodArgumentNotValidException in ExceptionHandler

--- backend/src/main/java/toobia/se/inventory/exceptions/InventoryExceptionHandler.java
+++ backend/src/main/java/toobia/se/inventory/exceptions/InventoryExceptionHandler.java
@@ -1,32 +1,46 @@
 package toobia.se.inventory.exceptions;
 
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.ValidationException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
-public class InventoryExceptionHandler extends ResponseEntityExceptionHandler {
+public class InventoryExceptionHandler{
 
     @ExceptionHandler({InventoryResourceNotFound.class})
-    protected ResponseEntity handleMissing(RuntimeException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), HttpStatus.valueOf(404), request);
+    protected ResponseEntity<String> handleMissing(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.valueOf(404)).body(ex.getMessage());
     }
 
     @ExceptionHandler({InventoryResourceExists.class})
-    protected ResponseEntity handleAlreadyExists(RuntimeException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), HttpStatus.valueOf(409), request);
+    protected ResponseEntity<String> handleAlreadyExists(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.valueOf(409)).body(ex.getMessage());
     }
 
-    @ExceptionHandler({InventoryBadInput.class})
-    protected ResponseEntity handleBadInput(RuntimeException ex, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getMessage(),
-                new HttpHeaders(), HttpStatus.valueOf(400), request);
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<Object> handleBadInput(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        DefaultMessageSourceResolvable::getDefaultMessage,
+                        (existing, replacement) -> existing // Handle duplicate field names
+                ));
+        return ResponseEntity.status(HttpStatus.valueOf(400))
+                .body(errors);
     }
 
 }
