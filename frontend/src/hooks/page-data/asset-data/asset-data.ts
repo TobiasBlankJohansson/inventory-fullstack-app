96f9dbbe6e7da69ed807aaff49f9a4b2e5072f1b
TobiasBlankJohansson <Tobias@annito.se>
2025-03-26 11:07:39 +0100

    faet:add edit-equipment

--- frontend/src/hooks/page-data/asset-data/asset-data.ts
+++ frontend/src/hooks/page-data/asset-data/asset-data.ts
@@ -1,17 +1,14 @@
-import {useGetEquipment, useGetItems, useGetResponsible, useGetStorage} from "@/hooks";
+import {useGetEquipment} from "@/hooks";
 import {useState} from "react";
 import {useEditItem} from "@/hooks/create-item/edit-item.ts";
 
-export const useItemEditorData = (id: string) => {
-  const {items, setItems} = useGetItems();
-  const {storageArea} = useGetStorage();
-  const {equipment} = useGetEquipment();
-  const {responsible} = useGetResponsible();
+export const useAssetData = (id: string) => {
+  const {equipment: equipmentList, setEquipment} = useGetEquipment();
   const [edit, setEdit] = useState(false);
   const {item, onSubmit, onDelete} = useEditItem(id, items, setItems, equipment, setEdit);
 
   return {
-    options: {equipment, responsible, storageArea},
+    equipmentList,
     item, edit, setEdit, onSubmit, onDelete
   };
 };
--- frontend/src/page/Asset.tsx
+++ frontend/src/page/Asset.tsx
@@ -1,30 +1,29 @@
 import {BodyContainer, EditItemForm, FormField, Navbar, ScreenContainer} from "@/components";
-import {FORM_FIELDS_ITEM} from "@/constants.ts";
-import {fromItemToFormField} from "@/types";
-import {option} from "@/util";
+import {FORM_FIELDS_EQUIPMENT} from "@/constants.ts";
+import {useAssetData} from "@/hooks/page-data/asset-data";
 
 export const Asset = () => {
-  
+  const {} = useAssetData();
+
   return (
     <ScreenContainer>
       <Navbar currentPageName="item"></Navbar>
       <BodyContainer>
         <section
           className="bg-white my-5 h-full rounded-xl min-[768px]:mx-20 min-[1024px]:mx-60 overflow-scroll scrollbar-hide">
-          {item && (
+          {equipment && (
             <EditItemForm
               onSubmit={onSubmit}
-              item={item}
+              item={equipment}
               edit={edit}
               onDelete={onDelete}
               setEdit={setEdit}
             >
-              {FORM_FIELDS_ITEM.map((field) => (
+              {FORM_FIELDS_EQUIPMENT.map((field) => (
                 <FormField
                   key={field.key}
                   field={field}
-                  value={fromItemToFormField(item)[field.key]}
-                  options={option(field.label, options)}
+                  value={equipment[field.key]}
                   edit={!edit}
                 />
               ))}
