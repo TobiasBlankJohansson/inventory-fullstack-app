33edb335c3c54f7f552e3f9c3c14adecb23d8532
TobiasBlankJohansson <Tobias@annito.se>
2025-03-27 15:33:02 +0100

    feat:add CreateAsset

--- frontend/src/page/AssetTable.tsx
+++ frontend/src/page/AssetTable.tsx
@@ -1,25 +1,45 @@
-import {BodyContainer, Navbar, renderTableHeaders, ScreenContainer, Table} from "@/components";
+import {BodyContainer, CreateAsset, Navbar, renderTableHeaders, ScreenContainer, Table} from "@/components";
 import {renderTableAsset} from "@/components/tabel/RenderAsset.tsx";
-import {useGetEquipment, useOrderItem} from "@/hooks";
-import {getTableHeaders} from "@/util";
+import {useGetEquipment, useOrderItem, usePostEquipment, useSaveAsset} from "@/hooks";
+import {getTableHeaders, openModal} from "@/util";
 import {Equipment} from "@/types";
+import {useState} from "react";
 
 export const AssetTable = () => {
-  const {orderObject, asset} = useEquipmentTable();
+  const {orderObject, asset, saveAsset, checkedItems} = useEquipmentTable();
 
   return <ScreenContainer>
     <Navbar currentPageName="item"></Navbar>
     <BodyContainer>
       <Table
-        renderHeadersInTable={renderTableHeaders(getTableHeaders(asset), orderObject.setOrder, orderObject.order, true, "equipment")}
+        renderHeadersInTable={
+          renderTableHeaders(getTableHeaders(asset),
+            orderObject.setOrder,
+            orderObject.order,
+            true,
+            "equipment",
+            checkedItems,
+            () => {
+            },
+            () => openModal("equipment"))}
         renderItemInTable={renderTableAsset(orderObject.orderItems(asset, orderObject.order) as Equipment[], "equipment", true)}></Table>
     </BodyContainer>
+    <CreateAsset
+      saveAsset={saveAsset}
+      dialogName={"equipment"}
+      useId={true}
+      useTable={true}
+    ></CreateAsset>
   </ScreenContainer>
 }
 
 const useEquipmentTable = () => {
-  const {equipment} = useGetEquipment();
+  const {equipment, setEquipment} = useGetEquipment();
   const orderObject = useOrderItem()
+  const {mutateAsync: mutateEquipment} = usePostEquipment();
+  const [checkedItems, setCheckedItems] = useState<string[]>([]);
 
-  return {asset: equipment, orderObject}
+  const saveAssetEquipment = useSaveAsset(setEquipment, mutateEquipment, equipment)
+
+  return {asset: equipment, orderObject, saveAsset: saveAssetEquipment, checkedItems}
 }
