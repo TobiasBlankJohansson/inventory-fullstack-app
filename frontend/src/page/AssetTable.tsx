db8ec699ba62c589c091c3abfaad671beb645a2e
TobiasBlankJohansson <Tobias@annito.se>
2025-03-27 15:10:09 +0100

    feat:add useEquipmentTable

--- frontend/src/page/AssetTable.tsx
+++ frontend/src/page/AssetTable.tsx
@@ -5,15 +5,21 @@ import {getTableHeaders} from "@/util";
 import {Equipment} from "@/types";
 
 export const AssetTable = () => {
-  const {equipment} = useGetEquipment();
-  const {setOrder, order, orderItems} = useOrderItem()
+  const {orderObject, asset} = useEquipmentTable();
 
   return <ScreenContainer>
     <Navbar currentPageName="item"></Navbar>
     <BodyContainer>
       <Table
-        renderHeadersInTable={renderTableHeaders(getTableHeaders(equipment), setOrder, order, true, "equipment")}
-        renderItemInTable={renderTableAsset(orderItems(equipment, order) as Equipment[], "equipment", true)}></Table>
+        renderHeadersInTable={renderTableHeaders(getTableHeaders(asset), orderObject.setOrder, orderObject.order, true, "equipment")}
+        renderItemInTable={renderTableAsset(orderObject.orderItems(asset, orderObject.order) as Equipment[], "equipment", true)}></Table>
     </BodyContainer>
   </ScreenContainer>
+}
+
+const useEquipmentTable = () => {
+  const {equipment} = useGetEquipment();
+  const orderObject = useOrderItem()
+
+  return {asset: equipment, orderObject}
 }
