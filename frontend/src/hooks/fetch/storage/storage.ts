34ce99b9c725b588cbad42942d9b3cddd08598f9
TobiasBlankJohansson <Tobias@annito.se>
2025-03-28 10:30:39 +0100

    feat:add usePutStorage, useDeleteStorage and useStorage

--- frontend/src/hooks/fetch/storage/storage.ts
+++ frontend/src/hooks/fetch/storage/storage.ts
@@ -1,9 +1,22 @@
 import {getStorageArea, postStorageArea} from "@/api/StorageFetch";
 
 import {useMutation} from "@tanstack/react-query";
+import {deleteStorage, putStorage} from "@/api/StorageFetch.ts";
 import {useGet} from "@/hooks";
 
 const queryKey = "storageArea";
+
+export const useStorage = () => {
+  const {storageArea, setStorageArea} = useGetStorage();
+  return {
+    asset: storageArea,
+    setAsset: setStorageArea,
+    useGet: useGetStorage,
+    usePost: usePostStorage,
+    useDelete: useDeleteStorage,
+    usePut: usePutStorage
+  }
+}
 
 export const useGetStorage = () => {
   const {data: storageArea, set: setStorageArea} = useGet(getStorageArea, queryKey);
@@ -15,3 +28,15 @@ export const usePostStorage = () => {
     mutationFn: postStorageArea,
   });
 }
+
+export const useDeleteStorage = () => {
+  return useMutation({
+    mutationFn: deleteStorage,
+  });
+};
+
+export const usePutStorage = () => {
+  return useMutation({
+    mutationFn: putStorage,
+  });
+};
