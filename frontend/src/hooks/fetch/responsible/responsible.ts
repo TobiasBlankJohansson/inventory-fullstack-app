beab05d777bdcc6f7c344ac03637f268faceb0a0
TobiasBlankJohansson <Tobias@annito.se>
2025-03-28 10:28:28 +0100

    faet:add useResponsible, useDeleteResponsible, usePutResponsible

--- frontend/src/hooks/fetch/responsible/responsible.ts
+++ frontend/src/hooks/fetch/responsible/responsible.ts
@@ -1,15 +1,40 @@
-import {getResponsible, postResponsible} from "@/api/ResponsibleFetch";
+import {deleteResponsible, getResponsible, postResponsible, putResponsible} from "@/api/ResponsibleFetch";
 import {useMutation} from "@tanstack/react-query";
 import {useGet} from "@/hooks";
 
 const queryKey = "responsible";
 
+export const useResponsible = () => {
+  const {responsible, setResponsible} = useGetResponsible();
+  return {
+    asset: responsible,
+    setAsset: setResponsible,
+    useGet: useGetResponsible,
+    usePost: usePostResponsible,
+    useDelete: useDeleteResponsible,
+    usePut: usePutResponsible
+  }
+}
+
 export const useGetResponsible = () => {
   const {data: responsible, set: setResponsible} = useGet(getResponsible, queryKey);
   return {responsible, setResponsible};
 };
+
 export const usePostResponsible = () => {
   return useMutation({
     mutationFn: postResponsible,
   });
 }
+
+export const useDeleteResponsible = () => {
+  return useMutation({
+    mutationFn: deleteResponsible,
+  });
+};
+
+export const usePutResponsible = () => {
+  return useMutation({
+    mutationFn: putResponsible,
+  });
+};
