fe2ad768326b74506a6e1d8dc2ef55f65bb36fcf
TobiasBlankJohansson <Tobias@annito.se>
2025-03-28 11:25:38 +0100

    refa:refactor so useAssetData gets after type

--- frontend/src/page/AssetEdit.tsx
+++ frontend/src/page/AssetEdit.tsx
@@ -2,7 +2,7 @@ import {BodyContainer, EditItemForm, FormField, Navbar, ScreenContainer} from "@
 import {FORM_FIELDS_ASSET} from "@/constants.ts";
 import {useLocation} from "react-router-dom";
 import {FormFieldItem} from "@/types";
-import {useDeleteEquipment, useEditAsset, useGetEquipment, usePutEquipment} from "@/hooks";
+import {useEditAsset, useEquipment, useResponsible, useStorage} from "@/hooks";
 import {useState} from "react";
 import {capitalize} from "@/util";
 
@@ -11,7 +11,7 @@ export const AssetEdit = () => {
   const type = capitalize(
     new URLSearchParams(useLocation().search).get("type") as string
   );
-  const {equipment, edit, setEdit, onDelete, onSubmit} = useAssetData(id, type);
+  const {asset, edit, setEdit, onDelete, onSubmit} = useAssetData(id, type);
 
   return (
     <ScreenContainer>
@@ -19,10 +19,10 @@ export const AssetEdit = () => {
       <BodyContainer>
         <section
           className="bg-white my-5 h-full rounded-xl min-[768px]:mx-20 min-[1024px]:mx-60 overflow-scroll scrollbar-hide">
-          {equipment && (
+          {asset && (
             <EditItemForm
               onSubmit={onSubmit}
-              name={"Equipment"}
+              name={type}
               edit={edit}
               onDelete={onDelete}
               setEdit={setEdit}
@@ -31,7 +31,7 @@ export const AssetEdit = () => {
                 <FormField
                   key={field.key}
                   field={field}
-                  value={(equipment as FormFieldItem)[field.key]}
+                  value={(asset as FormFieldItem)[field.key]}
                   edit={!edit}
                 />
               ))}
@@ -44,15 +44,22 @@ export const AssetEdit = () => {
 }
 
 export const useAssetData = (id: string, type: string) => {
-  const {equipment: equipmentList, setEquipment} = useGetEquipment();
+  const hooksMap = {
+    Equipment: useEquipment,
+    Responsible: useResponsible,
+    Storage: useStorage,
+  };
+
+  const hook = hooksMap[type as keyof typeof hooksMap];
+  const {asset: assetList, setAsset, usePost, useDelete} = hook();
   const [edit, setEdit] = useState(false);
   const {
     asset,
     onSubmit,
     onDelete
-  } = useEditAsset(id, equipmentList, setEquipment, setEdit, usePutEquipment(), useDeleteEquipment());
+  } = useEditAsset(id, assetList, setAsset, setEdit, usePost(), useDelete());
 
   return {
-    equipmentList, equipment: asset, edit, setEdit, onSubmit, onDelete
+    asset, edit, setEdit, onSubmit, onDelete
   };
 };
