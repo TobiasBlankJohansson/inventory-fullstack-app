afd176c333db0cf956873d68c90624f464f8f938
TobiasBlankJohansson <Tobias@annito.se>
2025-03-28 10:23:43 +0100

    feat:add putResponsible and deleteResponsible

--- frontend/src/api/ResponsibleFetch.ts
+++ frontend/src/api/ResponsibleFetch.ts
@@ -8,15 +8,27 @@ export async function getResponsible(): Promise<Responsible[]> {
   );
 }
 
-export async function postResponsible(responsible: {
-  name: string;
-  id: string;
-}): Promise<Responsible> {
+export async function postResponsible(responsible: Responsible): Promise<Responsible> {
   return await fetch(BACKEND_URL + "/api/responsible", {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({name: responsible.name}),
   }).then((res) => res.json());
+}
+
+export async function putResponsible(responsible: Responsible): Promise<Responsible> {
+  return fetch(BACKEND_URL + "/api/responsible", {
+    method: "PUT",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(responsible),
+  }).then((res) => res.json());
+}
+
+export async function deleteResponsible(id: string): Promise<boolean> {
+  const response = await fetch(BACKEND_URL + "/api/responsible/" + id, {
+    method: "DELETE",
+  });
+  return response.ok;
 }
 
 export const mockDataResponsible: Responsible[] = [
