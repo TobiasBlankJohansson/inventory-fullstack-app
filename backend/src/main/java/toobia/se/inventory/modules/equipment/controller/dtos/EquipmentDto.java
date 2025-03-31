acfd7d866f0da1a5bc5c2c11633d6e33d32dd203
TobiasBlankJohansson <Tobias@annito.se>
2025-03-31 09:38:07 +0200

    refa:refactor id length of equipment

--- backend/src/main/java/toobia/se/inventory/modules/equipment/controller/dtos/EquipmentDto.java
+++ backend/src/main/java/toobia/se/inventory/modules/equipment/controller/dtos/EquipmentDto.java
@@ -1,18 +1,18 @@
 package toobia.se.inventory.modules.equipment.controller.dtos;
 
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import toobia.se.inventory.modules.equipment.model.Equipment;
 
 import java.util.List;
 
 public record EquipmentDto(
-        @Pattern(regexp = "\\d{6}", message = "Id need to be numbers and 6 characters long")
-         String id,
+        @Pattern(regexp = "\\d{4}", message = "Id need to be numbers and 4 characters long")
+        String id,
         String name) {
     public static EquipmentDto from(Equipment equipment) {
-        return new EquipmentDto(equipment.getEquipmentId(),equipment.getEquipmentName());
+        return new EquipmentDto(equipment.getEquipmentId(), equipment.getEquipmentName());
     }
+
     public static List<EquipmentDto> listFrom(List<Equipment> equipments) {
         return equipments.stream().map(EquipmentDto::from).toList();
     }
