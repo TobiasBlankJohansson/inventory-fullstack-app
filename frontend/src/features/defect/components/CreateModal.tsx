1fdbc094bacf706ce429a9a694cf537755485d6c
TobiasBlankJohansson <Tobias@annito.se>
2025-04-10 08:41:49 +0200

    refa:refactor ShowDefectModel attributes

--- frontend/src/features/defect/components/CreateModal.tsx
+++ frontend/src/features/defect/components/CreateModal.tsx
@@ -56,119 +56,120 @@ export const DefectReportModal = () => {
   };
 
   return (
-    <div>
-      <dialog id="defect_report_modal" className="modal">
-        <div className="modal-box bg-white rounded-lg p-8 shadow-lg max-w-lg w-full">
-          <h3 className="font-bold text-3xl text-center mb-6 decoration-gray-300">
-            Defect report
-          </h3>
+    <dialog id="defect_report_modal" className="modal">
+      <div className="modal-box bg-white rounded-lg p-8 shadow-lg max-w-lg w-full">
+        <h3 className="font-bold text-3xl text-center mb-6 decoration-gray-300">
+          Defect report
+        </h3>
 
-          <form method="dialog" onSubmit={handleSubmit} className="space-y-4">
-            <section className="grid grid-cols-2 gap-4">
-              <div>
-                <label htmlFor="responsible" className="block text-sm font-medium text-gray-700">
-                  Responsible
-                </label>
-                <select
-                  id="responsible"
-                  className="select select-bordered w-full max-w-xs mt-1"
-                  value={defectReport.responsible}
-                  onChange={(e) => setDefectReport(prev => ({...prev, responsible: e.target.value}))}
-                >
-                  <option value="" disabled>
-                    Select here
-                  </option>
-                  {responsible.map((responsible) => (
-                    <option key={responsible.id} value={responsible.id}>{responsible.name}</option>
-                  ))}
-                </select>
-              </div>
-              <div>
-                <label htmlFor="date" className="block text-sm font-medium text-gray-700">
-                  Date
-                </label>
-                <input
-                  type="date"
-                  id="date"
-                  placeholder="Value"
-                  className="input input-bordered w-full max-w-xs mt-1"
-                  value={defectReport.date}
-                  onChange={(e) => setDefectReport(prev => ({...prev, date: e.target.value}))}
-                />
-              </div>
-            </section>
-
-            <section className="grid grid-cols-2 gap-4">
-              <div>
-                <label htmlFor="filed" className="block text-sm font-medium text-gray-700">
-                  Filed by
-                </label>
-                <input
-                  type="text"
-                  id="filed"
-                  placeholder="Type here"
-                  className="input input-bordered w-full max-w-xs mt-1"
-                  value={defectReport.filed}
-                  onChange={(e) => setDefectReport(prev => ({...prev, filed: e.target.value}))}
-                />
-              </div>
-              <div>
-                <label htmlFor="equipment" className="block text-sm font-medium text-gray-700">
-                  Equipment
-                </label>
-                <select
-                  id="equipment"
-                  className="select select-bordered w-full max-w-xs mt-1"
-                  value={defectReport.equipment}
-                  onChange={(e) => setDefectReport(prev => ({...prev, equipment: e.target.value}))}
-                >
-                  <option value="" disabled>
-                    Select here
-                  </option>
-                  {equipment.map((equipment) => (
-                    <option key={equipment.id} value={equipment.id}>{equipment.name}</option>
-                  ))}
-                </select>
-              </div>
-            </section>
-
+        <form method="dialog" onSubmit={handleSubmit} className="space-y-4">
+          <section className="grid grid-cols-2 gap-4">
             <div>
-              <label htmlFor="defect" className="block text-sm font-medium text-gray-700">
-                Defect, deficiency
+              <label htmlFor="responsible" className="block text-sm font-medium text-gray-700">
+                Responsible
               </label>
-              <textarea
-                id="defect"
-                placeholder="Type here"
-                className="textarea textarea-bordered w-full mt-1 resize-none"
-                value={defectReport.defect}
-                onChange={(e) => setDefectReport(prev => ({...prev, defect: e.target.value}))}
+              <select
+                id="responsible"
+                className="select select-bordered w-full max-w-xs mt-1"
+                value={defectReport.responsible}
+                onChange={(e) => setDefectReport(prev => ({...prev, responsible: e.target.value}))}
+              >
+                <option value="" disabled>
+                  Select here
+                </option>
+                {responsible.map((responsible) => (
+                  <option key={responsible.id} value={responsible.id}>{responsible.name}</option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label htmlFor="date" className="block text-sm font-medium text-gray-700">
+                Date
+              </label>
+              <input
+                type="date"
+                id="date"
+                placeholder="Value"
+                className="input input-bordered w-full max-w-xs mt-1"
+                value={defectReport.date}
+                onChange={(e) => setDefectReport(prev => ({...prev, date: e.target.value}))}
               />
             </div>
+          </section>
 
-            <p className="text-sm text-gray-500">
-              Defect : e.g "Axe , Defect"
-              <br/>
-              Deficiency : e.g "Spirit Burner, Deficiency"
-            </p>
-
-            <div className="modal-action justify-between">
-              <button type="button" className="btn" onClick={handleCloseModal}>
-                Cancel
-              </button>
-              <div>
-                <button type="submit" className="btn bg-button_primary text-white hover:bg-button_primary_hover mr-2"
-                        onClick={() => setAddAnotherOne(true)}>
-                  Create another one
-                </button>
-                <button type="submit" className="btn bg-button_secondary text-white hover:bg-button_secondary_hover">
-                  Create
-                </button>
-              </div>
-
+          <section className="grid grid-cols-2 gap-4">
+            <div>
+              <label htmlFor="filed" className="block text-sm font-medium text-gray-700">
+                Filed by
+              </label>
+              <input
+                type="text"
+                id="filed"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs mt-1"
+                value={defectReport.filed}
+                onChange={(e) => setDefectReport(prev => ({...prev, filed: e.target.value}))}
+              />
             </div>
-          </form>
-        </div>
-      </dialog>
-    </div>
+            <div>
+              <label htmlFor="equipment" className="block text-sm font-medium text-gray-700">
+                Equipment
+              </label>
+              <select
+                id="equipment"
+                className="select select-bordered w-full max-w-xs mt-1"
+                value={defectReport.equipment}
+                onChange={(e) => setDefectReport(prev => ({...prev, equipment: e.target.value}))}
+              >
+                <option value="" disabled>
+                  Select here
+                </option>
+                {equipment.map((equipment) => (
+                  <option key={equipment.id} value={equipment.id}>{equipment.name}</option>
+                ))}
+              </select>
+            </div>
+          </section>
+
+          <div>
+            <label htmlFor="defect" className="block text-sm font-medium text-gray-700">
+              Defect, deficiency
+            </label>
+            <textarea
+              id="defect"
+              placeholder="Type here"
+              className="textarea textarea-bordered w-full mt-1 resize-none"
+              value={defectReport.defect}
+              onChange={(e) => setDefectReport(prev => ({...prev, defect: e.target.value}))}
+            />
+          </div>
+
+          <p className="text-sm text-gray-500">
+            Defect : e.g "Axe , Defect"
+            <br/>
+            Deficiency : e.g "Spirit Burner, Deficiency"
+          </p>
+
+          <div className="modal-action justify-between">
+            <button type="button" className="btn" onClick={handleCloseModal}>
+              Cancel
+            </button>
+            <div>
+              <button type="submit" className="btn bg-button_primary text-white hover:bg-button_primary_hover mr-2"
+                      onClick={() => setAddAnotherOne(true)}>
+                Create another one
+              </button>
+              <button type="submit" className="btn bg-button_secondary text-white hover:bg-button_secondary_hover">
+                Create
+              </button>
+            </div>
+
+          </div>
+        </form>
+      </div>
+      <form method="dialog" className="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
   );
 };
--- frontend/src/features/defect/components/ShowDefectModel.tsx
+++ frontend/src/features/defect/components/ShowDefectModel.tsx
@@ -4,66 +4,56 @@ export const ShowDefectModel = ({defect}: { defect: Defect }) => {
   return (
     <dialog id="showDefect" className="modal">
       <div className="modal-box">
-        <h3 className="font-bold text-2xl mb-8">Defect report</h3>
+        <h3 className="font-bold text-3xl mb-8 text-center">Defect report</h3>
 
         <div className="grid grid-cols-2 gap-x-8 gap-y-4">
-          <div className="mb-4">
-            <div className="form-control w-full ">
-              <label className="label">
-                <span className="label-text">Responsible</span>
-              </label>
-              <div className="border rounded-md p-2 bg-base-100">
-                {defect.responsible}
-              </div>
-            </div>
-          </div>
-
-          <div className="mb-4">
-            <div className="form-control w-full ">
-              <label className="label">
-                <span className="label-text">Date</span>
-              </label>
-              <div className="border rounded-md p-2 bg-base-100">
-                {defect.date}
-              </div>
-            </div>
-          </div>
-
-          <div className="mb-4">
-            <div className="form-control w-full ">
-              <label className="label">
-                <span className="label-text">Filed by</span>
-              </label>
-              <div className="border rounded-md p-2 bg-base-100">
-                {defect.filed}
-              </div>
-            </div>
-          </div>
-
-          <div className="mb-4">
-            <div className="form-control w-full ">
-              <label className="label">
-                <span className="label-text">Equipment</span>
-              </label>
-              <div className="border rounded-md p-2 bg-base-100"> {/* Simulate input look */}
-                {defect.equipment}
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="mb-4">
-          <div className="form-control w-full ">
+          <section className="form-control w-full ">
             <label className="label">
-              <span className="label-text">Fel, brist</span>
+              <span className="label-text">Responsible</span>
             </label>
-            <textarea
-              className="textarea textarea-bordered w-full h-24"
-              readOnly
-              value={defect.defect}
-            ></textarea>
-          </div>
+            <div className="border rounded-md p-2 bg-base-100">
+              {defect.responsible}
+            </div>
+          </section>
+
+          <section className="form-control w-full ">
+            <label className="label">
+              <span className="label-text">Date</span>
+            </label>
+            <div className="border rounded-md p-2 bg-base-100">
+              {defect.date}
+            </div>
+          </section>
+
+          <section className="form-control w-full ">
+            <label className="label">
+              <span className="label-text">Filed by</span>
+            </label>
+            <div className="border rounded-md p-2 bg-base-100">
+              {defect.filed}
+            </div>
+          </section>
+
+          <section className="form-control w-full ">
+            <label className="label">
+              <span className="label-text">Equipment</span>
+            </label>
+            <div className="border rounded-md p-2 bg-base-100"> {/* Simulate input look */}
+              {defect.equipment}
+            </div>
+          </section>
         </div>
+
+        <section className="form-control w-full ">
+          <label className="label">
+            <span className="label-text">Fel, brist</span>
+          </label>
+          <textarea
+            className="textarea textarea-bordered w-full h-24"
+            readOnly
+            value={defect.defect}
+          ></textarea>
+        </section>
       </div>
       <form method="dialog" className="modal-backdrop">
         <button>close</button>
