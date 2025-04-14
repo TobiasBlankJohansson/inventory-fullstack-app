3b543215070dbefad7a7d163177299810cca3632
TobiasBlankJohansson <Tobias@annito.se>
2025-04-14 09:53:22 +0200

    refa:add create methode in DefectServuce

--- backend/src/main/java/toobia/se/inventory/modules/defect/service/DefectService.java
+++ backend/src/main/java/toobia/se/inventory/modules/defect/service/DefectService.java
@@ -1,20 +1,26 @@
 package toobia.se.inventory.modules.defect.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import toobia.se.inventory.exceptions.InventoryResourceNotFound;
 import toobia.se.inventory.modules.defect.model.Defect;
 import toobia.se.inventory.modules.defect.repository.DefectRepository;
+import toobia.se.inventory.modules.equipment.model.Equipment;
+import toobia.se.inventory.modules.equipment.service.EquipmentService;
+import toobia.se.inventory.modules.responsible.model.Responsible;
+import toobia.se.inventory.modules.responsible.service.ResponsibleService;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class DefectService {
     private final DefectRepository defectRepository;
-
-    public DefectService(DefectRepository defectRepository) {
-        this.defectRepository = defectRepository;
-    }
+    private final ResponsibleService responsibleService;
+    private final EquipmentService equipmentService;
+    private Equipment equipment;
+    private Responsible responsible;
 
     public List<Defect> getList() {
         return defectRepository.findAll();
@@ -22,6 +28,13 @@ public class DefectService {
 
     public Defect getById(UUID id) {
         return defectRepository.findById(id).orElseThrow(() -> new InventoryResourceNotFound("Defect not found"));
+    }
+
+    public Defect create(String date, String filed, String status, String defect, String equipmentId, String responsibleId) {
+        var equipment = equipmentService.readEquipment(equipmentId);
+        var responsible = responsibleService.readResponsible(UUID.fromString(responsibleId));
+        var defectObj = new Defect(date, filed, status, defect, equipment, responsible);
+        return defectRepository.save(defectObj);
     }
 
 }
