9f122287c5cbc8b842e10e6e823786a2d6b35de9
TobiasBlankJohansson <Tobias@annito.se>
2025-04-16 15:05:35 +0200

    refa:add top buttons

--- frontend/src/features/custom-list/create-custom-list.tsx
+++ frontend/src/features/custom-list/create-custom-list.tsx
@@ -1,8 +1,31 @@
-import {Navbar, ScreenContainer} from "@/components";
+import {BodyContainer, FullHeightButton, Navbar, ScreenContainer, Search} from "@/components";
+import {Filter} from "@/features";
+import {openModal} from "@/lib";
 
 export const CreateCustomList = () => {
   return <ScreenContainer>
     <Navbar currentPage={2} currentPageName={'CreateCustomList'}/>
-
+    <BodyContainer>
+      <div className={"h-10 grid grid-cols-2 gap-2"}>
+        <section className={"w-full"}>
+          <div className="grid grid-cols-2 gap-2 h-full">
+            <Search setSearch={() => ""}/>
+            <FullHeightButton onClick={() => openModal("filter")}>Filter</FullHeightButton>
+          </div>
+        </section>
+        <section className={"w-full"}>
+          <div className="grid grid-cols-2 gap-2 h-full">
+            <FullHeightButton>Go to custom list</FullHeightButton>
+            <FullHeightButton>Save custom list</FullHeightButton>
+          </div>
+        </section>
+      </div>
+    </BodyContainer>
+    <Filter
+      setStorage={() => ""}
+      storage={["stuff"]}
+      setResponsible={() => ""}
+      responsible={[]}
+    />
   </ScreenContainer>;
 }
