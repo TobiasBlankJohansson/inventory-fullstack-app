65aea4b483d5f6e99874dd8c1da2f8480c8f2cf3
TobiasBlankJohansson <Tobias@annito.se>
2025-04-17 10:01:59 +0200

    feat:add logic for filter and search

--- frontend/src/features/custom-list/create-custom-list.tsx
+++ frontend/src/features/custom-list/create-custom-list.tsx
@@ -1,11 +1,14 @@
-import {BodyContainer, FullHeightButton, Navbar, ScreenContainer, Search} from "@/components";
-import {Filter, Item, useGetItems} from "@/features";
+import {BodyContainer, Button, FullHeightButton, Navbar, ScreenContainer, Search} from "@/components";
+import {Filter, Item, useFilterItems, useGetItems, useResponsible, useStorage} from "@/features";
 import {openModal} from "@/lib";
 
 export const CreateCustomList = () => {
   const {items} = useGetItems();
+  const {asset: storage} = useStorage();
+  const {asset: responsible} = useResponsible();
+  const {itemList, setSearch, setSelectedStorage, setSelectedResponsible} = useFilterItems(items);
 
-  const InventoryItem = (item: Item) => {
+  const InventoryItem = (item: Item, left = true) => {
     return (
       <tr key={item.id}>
         <td className="p-1 py-3 pl-3">
@@ -26,9 +29,9 @@ export const CreateCustomList = () => {
             </label></div>
         </td>
         <td className="p-1 text-center">
-          <button className="btn btn-xs h-7 bg-button_secondary hover:bg-button_secondary_hover">
-            <span className="text-white">→</span>
-          </button>
+          <Button className="btn-xs h-7 bg-button_secondary hover:bg-button_secondary_hover">
+            <span className="text-white">{left ? "→" : "←"}</span>
+          </Button>
         </td>
       </tr>
     );
@@ -40,7 +43,7 @@ export const CreateCustomList = () => {
       <div className={"flex flex-col gap-2 md:flex-row md:gap-10 h-full"}>
         <section className="w-full h-full flex flex-col">
           <div className="h-10 grid grid-cols-2 gap-2">
-            <Search setSearch={() => ""}/>
+            <Search setSearch={setSearch}/>
             <FullHeightButton onClick={() => openModal("filter")}>Filter</FullHeightButton>
           </div>
 
@@ -63,7 +66,7 @@ export const CreateCustomList = () => {
                 </tr>
                 </thead>
                 <tbody>
-                {items.map((item) => InventoryItem(item))}
+                {itemList.map((item) => InventoryItem(item))}
                 </tbody>
               </table>
             </div>
@@ -80,6 +83,12 @@ export const CreateCustomList = () => {
               <h2 className="text-white py-2 px-4 text-left text-sm font-medium">
                 Custom list
               </h2>
+              <Button
+                className={"flex justify-center px-5 w-fit mr-4 btn-xs text-white " +
+                  "bg-button_secondary border-button_secondary hover:bg-button_secondary_hover hover:border-button_secondary"}
+                onClick={() => openModal("defect_report_modal")}>
+                Print Custom List
+              </Button>
             </header>
 
             <div className="overflow-x-auto overflow-y-auto flex-1">
@@ -94,7 +103,7 @@ export const CreateCustomList = () => {
                 </tr>
                 </thead>
                 <tbody>
-
+                {items.map((item) => InventoryItem(item, false))}
                 </tbody>
               </table>
             </div>
@@ -103,10 +112,10 @@ export const CreateCustomList = () => {
       </div>
     </BodyContainer>
     <Filter
-      setStorage={() => ""}
-      storage={["stuff"]}
-      setResponsible={() => ""}
-      responsible={[]}
+      setStorage={setSelectedStorage}
+      storage={storage.map((storage) => storage.name)}
+      setResponsible={setSelectedResponsible}
+      responsible={responsible.map((responsible) => responsible.name)}
     />
   </ScreenContainer>;
 }
